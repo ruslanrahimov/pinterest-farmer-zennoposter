3be2c609546e31f76ba2bfdb94c6cbefac39a264
Ruslan Rahimov <rahimoff.tr@gmail.com>
2024-09-27 22:09:06 +0300

    add basic profiles setup functionality

--- ZennoPosterSolution/ZennoPosterProject/Program.cs
+++ ZennoPosterSolution/ZennoPosterProject/Program.cs
@@ -33,77 +33,14 @@ namespace ZennoPosterProject
         /// <returns>Код выполнения скрипта</returns>		
         public int Execute(Instance instance, IZennoPosterProjectModel project)
         {
-            instance.ClearCache();
+            Puppeteer page = new Puppeteer(instance, project);
+            string website = project.Variables["website"].Value;
 
-            string threadid = project.Variables["threadid"].Value;
+            HtmlElement websiteInput = page.WaitXpath("//input[@id=\"website_url\"]");
+            page.Type(websiteInput, website);
 
-            lock (SyncObject)
-            {
-                //Получаем путь к профиль файлу
 
-                string workDirPath = project.Variables["workDirPath"].Value;
 
-                List<string> zpprofilesTemp = new List<string>();
-                string zpprofile = String.Empty;
-                string[] zpprofilesArray = Directory.GetFiles(workDirPath, "*.zpprofile", SearchOption.TopDirectoryOnly).OrderBy(d => new FileInfo(d).LastWriteTime).ToArray();
-
-                if (zpprofilesArray.Length == 0)
-                {
-                    throw new Exception($@"Папка c аккаунтами пуста {workDirPath}");
-                }
-
-                zpprofilesTemp.AddRange(zpprofilesArray);
-                zpprofile = zpprofilesTemp[0];
-                FileInfo fileInfo = new FileInfo(zpprofile);
-
-                //Сохраняем переменные которые есть в Профиле для дальнейшего пересохранения
-                string zpproileToExractDirName = fileInfo.Name.Replace(".zpprofile", "");
-
-                string extractPath = $@"{project.Directory}\extract\{zpproileToExractDirName}"; // Путь для извлечения
-
-                if (Directory.Exists(extractPath)) // Проверяем, существует ли директория
-                {
-                    Directory.Delete(extractPath, true); // Если существует - удаляем, иначе не даст разархивировать
-                }
-
-                ZipFile.ExtractToDirectory(zpprofile, extractPath); // Извлекаем содержимое архива
-
-                string variablesXML = File.ReadAllText(extractPath + @"\variables.zpdata"); // Читаем переменные из variables.zpdata
-
-                project.Xml.FromString(variablesXML);
-
-                // Проходимся по всем сохраненным в профиле переменным и сохранем их в список
-                for (int i = 0; i < project.Xml.SaveVariablesData.Variables.VariablesData.Count; i++)
-                {
-                    string variableName = project.Xml.SaveVariablesData.Variables.VariablesData[i].VariableName.Value;
-                    project.Lists["DefaultVariablesToSave"].Add(variableName);
-
-                }
-
-                Directory.Delete(extractPath, true); //Удаляем папку с извлеченными данными
-
-                //Загружаем профиль
-                project.SendInfoToLog($"{threadid}: Беру в работу профиль - {fileInfo.Name}", true);
-                project.Profile.Load(zpprofile, true);
-
-                //Перемещаем файл в папку temp
-                string zpprofilesTempFolderPath = $@"{project.Directory}\src\zpprofiles-temp";
-
-                if (!Directory.Exists(zpprofilesTempFolderPath)) //Проверяем имеется ли папка zpprofiles_temp
-                {
-                    Directory.CreateDirectory(zpprofilesTempFolderPath); // Создаем если нет
-                }
-
-                if (File.Exists($@"{zpprofilesTempFolderPath}\{fileInfo.Name}")) //Проверяем нет ли файла в папке перед перемещением
-                {
-                    File.Delete($@"{zpprofilesTempFolderPath}\{fileInfo.Name}");
-                }
-
-                File.Move(zpprofile, $@"{zpprofilesTempFolderPath}\{fileInfo.Name}");
-
-                project.Variables["zpprofileName"].Value = fileInfo.Name;
-
-            }
 
             return 0;
         }
