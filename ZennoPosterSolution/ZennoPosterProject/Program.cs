f388433c621550922c2f2723289bd04132255d16
Ruslan Rahimov <rahimoff.tr@gmail.com>
2024-10-02 19:12:10 +0300

    create new pin mode

--- ZennoPosterSolution/ZennoPosterProject/Program.cs
+++ ZennoPosterSolution/ZennoPosterProject/Program.cs
@@ -34,51 +34,13 @@ namespace ZennoPosterProject
         /// <returns>Код выполнения скрипта</returns>		
         public int Execute(Instance instance, IZennoPosterProjectModel project)
         {
-            
-
-            string accountType = project.Variables["accountType"].Value;
-
-            if(accountType == "business")
-            {
-                Puppeteer page = new Puppeteer(instance, project);
-                Log log = new Log(project);
-
-                // Получаем и задаем параметры загружаемого файла
-
-                int randomFileIndex;
-                string threadid = project.Variables["threadid"].Value;
-
-                string coverImagesDirPath = project.Variables["coverImagesDirPath"].Value;
-
-                string[] coverImages = Directory.GetFiles(coverImagesDirPath, "*.*", SearchOption.AllDirectories);
-
-                if (coverImages.Length == 0)
-                {
-                    log.Print($"{threadid}: Папка с фотографиями пуста! Пропускаю данное действие.", "red"); // Реализовать скачивание фотки со стороннего сервиса
-                    return true;
-                }
-
-                randomFileIndex = Global.Variables.MainRandom.GetNext(0, coverImages.Length);
-
-                string imageFilePath = coverImages[randomFileIndex];
-
-                instance.SetFileUploadPolicy("ok", "");
-                instance.SetFilesForUpload(imageFilePath);
 
 
+            Puppeteer page = new Puppeteer(instance, project);
+            HtmlElement saveButton = page.WaitXpath("//button[@data-test-id=\"board-dropdown-save-button\"]");
+            page.Click(saveButton);
 
-                HtmlElement profileLink = page.WaitXpath("//div[@data-test-id=\"header-profile\"]//a");
-                page.Click(profileLink);
 
-                HtmlElement editCoverButton = page.WaitXpath("//div[@data-test-id=\"profile-cover-edit-button\"]/button");
-                page.Click(editCoverButton);
-
-                HtmlElement browse = page.WaitXpath("//input[@id=\"asset-picker-upload\"]");
-                page.Click(browse);
-
-                HtmlElement doneButton = page.WaitXpath("//div[text()=\"Done\"]/../..");
-                page.Click(doneButton);
-            }
 
             return 0;
         }
