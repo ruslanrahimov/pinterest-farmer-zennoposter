2e22b770f72452417655ef05b6862351a0ab4623
Ruslan Rahimov <rahimoff.tr@gmail.com>
2024-10-03 17:07:29 +0300

    need fix

--- ZennoPosterSolution/ZennoPosterProject/Program.cs
+++ ZennoPosterSolution/ZennoPosterProject/Program.cs
@@ -34,11 +34,51 @@ namespace ZennoPosterProject
         /// <returns>Код выполнения скрипта</returns>		
         public int Execute(Instance instance, IZennoPosterProjectModel project)
         {
+            //"accountType":"influencer_public_figure_or_celebrity"
 
+            //TODO добавить проверку под бизнес акк
 
+            Log log = new Log(project);
             Puppeteer page = new Puppeteer(instance, project);
-            HtmlElement saveButton = page.WaitXpath("//button[@data-test-id=\"board-dropdown-save-button\"]");
-            page.Click(saveButton);
+
+            string threadid = project.Variables["threadid"].Value;
+
+            if (page.IsDomTextExists("\"name\":\"UserSettingsResource\"") || page.IsDomTextExists("\"accountType\":\"influencer_public_figure_or_celebrity\""))
+            {
+                log.Print($"{threadid}: Проверка на чек пройдена.", "yellow");
+                return true; //TODO replace 1 with true;
+            }
+
+            log.Print($"{threadid}: Пробую авторизоваться...");
+
+            string login = project.Variables["login"].Value;
+            string password = project.Variables["password"].Value;
+
+            HtmlElement loginButton = page.WaitXpath("//div[@data-test-id=\"simple-login-button\"]/button");
+            page.Click(loginButton);
+
+            HtmlElement emailInput = page.WaitXpath("//input[@id=\"email\"]");
+            HtmlElement passwordInput = page.WaitXpath("//input[@id=\"password\"]");
+            HtmlElement submitButton = page.WaitXpath("//button[@type=\"submit\"]");
+
+            page.Type(emailInput, login);
+            Thread.Sleep(1000);
+            page.Type(passwordInput, password);
+            Thread.Sleep(1000);
+            page.Click(submitButton);
+
+            try
+            {
+                page.WaitXpath("//div[@role=\"alertdialog\"]", 20000);
+                log.Print($"{threadid}: Аккаунт забанен.", "red");
+                project.Variables["ban"].Value = "True";
+            }
+            catch
+            {
+                log.Print($"{threadid}: Проверка на чек пройдена.", "yellow");
+                return true;
+            }
+
 
 
 
